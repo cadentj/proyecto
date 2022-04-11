cc04f0b8c8bb95a4042f3635a141e0ae2afe8a7f
Caden <caden.juang@gmail.com>
2022-04-10 20:34:45 -0500

    Card Screen Work

---
+++ components/Announcements.js/AnnouncementsList.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { ScrollView, VStack, Center, useTheme, Heading, NativeBaseProvider } from "native-base";
+
+export default function CardView() {
+  const {
+    colors
+  } = useTheme();
+  return <Center>
+      <ScrollView maxW="300" h="80" _contentContainerStyle={{
+      px: "20px",
+      mb: "4",
+      minW: "72"
+    }}>
+        <Center mt="3" mb="4">
+          <Heading fontSize="xl">Cyan</Heading>
+        </Center>
+        <VStack flex="1">
+          {Object.keys(colors.cyan).map((key, index) => {
+          if (index >= 1 && index <= 5) return <Center py="4" bg={`cyan.${key}`}>
+                  {key}
+                </Center>;
+        })}
+        </VStack>
+        <Center mt="8" mb="4">
+          <Heading fontSize="xl">Yellow</Heading>
+        </Center>
+        <VStack flex="1">
+          {Object.keys(colors.cyan).map((key, index) => {
+          if (index >= 1 && index <= 5) return <Center py="4" bg={`yellow.${key}`}>
+                  {key}
+                </Center>;
+        })}
+        </VStack>
+        <Center mt="8" mb="4">
+          <Heading fontSize="xl"> Violet</Heading>
+        </Center>
+        <VStack flex="1">
+          {Object.keys(colors.violet).map((key, index) => {
+          if (index >= 1 && index <= 5) return <Center py="4" bg={`violet.${key}`}>
+                  {key}
+                </Center>;
+        })}
+        </VStack>
+      </ScrollView>
+    </Center>;
+}
